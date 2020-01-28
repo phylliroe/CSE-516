69842b56d6f5e1cfa95fb2ebb04c01e1d33d3594
andy <andy@localhost.localdomain>
2020-01-27 22:08:33 -0800

    lab 2

--- lab_2.m
+++ lab_2.m
@@ -26,31 +26,33 @@ X_norm = D(1,:) - mean(D(1,:));
 Y_norm = D(2,:) - mean(D(2,:));
 D_norm = [X_norm; Y_norm];
 
+% Get the covariance matrix, the eigenvectors, and the eigenvalues
 D_norm = transpose(D_norm)
 [m n] = size(D_norm)
 cov = (1/m)*(D_norm' * D_norm)
 [V D W] = eig(cov)
 
+% Plot noralized data and principle component
 plot(linspace(-2,2), V(:,1) * linspace(-2,2))
 hold on 
 scatter(X_norm, Y_norm, 'filled')
 title("Normalized Data With Primary Components")
 clf
 
+% Get the reduced data
 V_reduce = V(:,2)
 Z = D_norm * V_reduce
 plot(Z, 'bo')
 title("Data In New Space")
 clf
-
 scatter(Z, Z)
 title("Data With Reduced Dimension")
-
 hold on
+
+% Reduced data and original data
 scatter(X, Y, '+')
 title("Original Data With Transformed Data")
 legend("Reduced", "Original")
-clf
 
 %plot(new_D, "b+")
 
