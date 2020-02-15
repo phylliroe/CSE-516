b1efd8ad8c8a08c2b3de60bfe6d4972eb0ba635c
andy <andy@localhost.localdomain>
2020-02-15 12:13:42 -0800

    lab 4

--- lab_4/qlearning.m
+++ lab_4/qlearning.m
@@ -45,52 +45,86 @@ function M = init_grid(n)
   M = zeros(n);
 end
 
-function R = reward(n)
-  R = zeros((n^2), 4);
+%function R = reward(n)
+%  R = zeros((n^2), 4);
+%end 
+
+% Generate a random action based on the agents current row and column position. 
+function action = get_random_action(row, col, n)
+  x = 0;
+  if row == 1
+    if col == 1
+      x = setdiff(1:4, [1, 3]);
+    elseif col == n
+      x = setdiff(1:4, [1, 4]);
+    else
+      x = setdiff(1:4, [1]);
+    end 
+  elseif row == n
+    if col == 1
+      x = setdiff(1:4, [2, 3]);
+    elseif col == n
+      x = setdiff(1:4, [2, 4]);
+    else
+      x = setdiff(1:4, [2]);
+    end
+  elseif col == 1
+    x = setdiff(1:4, [3]);
+  elseif col == n
+    x = setdiff(1:4, [4]);
+  else 
+    x = setdiff(1:4, []);
+  end 
+    action = x(randi(numel(x)));   
 end 
 
-function action = get_random_action(row, col, n)
-  %action = randi([1,4], 1);
-  rand_action = randi([1,4], 1);
-  while rand_action ~= 0
-      %rand_action = randi([1,4], 1);
-      
-      if row == 1 && rand_action == 1
-          rand_action = 0;
-      elseif row == n && rand_action == 4
-          rand_action = 0;
-      end 
-      rand_action = randi([1,4], 1);
-    end 
-    
-    action = rand_action;
+% [1 2 3....n]
+function R = create_rewards(n, goal)
+   R = ones(1,(n^2)) * -1;
+   R(goal) = 10;
+end
+
+function [new_row, new_col] = next_state(action, row, col)
+  new_row = row;
+  new_col = col;
+  
+  switch action
+    case 1
+      new_row = new_row - 1;
+    case 2
+      new_row = new_row + 1;    
+    case 3
+      new_col = new_col - 1;
+    case 4
+      new_col = new_col + 1;
+   end 
 end 
 
 n = 4;
-start_state = 1;
+start_state = 4;
 goal = 11;
-row = 1;
-col = 1;
+row = 2;
+col = 2;
 
 grid_world = init_grid(n);
 Q = init_grid(n);
-R = reward(n);
+%R = reward(n);
 %state = state_num(row, col, n)
 %size(R)
-
-for i = 1:20
-  new_action = get_random_action(row, col, n)
-end 
+R = create_rewards(n, goal);
 
 
+%for i = 1:20
+ % curr_state = state_num(row, col, n);
+  %new_action = get_random_action(row, col, n)
+%end
+%x = setdiff(1:4, [3])
+%y = x(randi(numel(x)))
 
-
-
-
-
-
-
-
+current_state = start_state
+action = get_random_action(row, col, n)
+[new_row, new_col] = next_state(action, row, col)
+current_state = state_num(new_row, new_col, n)
 
 
 
